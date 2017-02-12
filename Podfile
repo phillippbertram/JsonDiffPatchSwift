639815ed9217eaa5a3cf7805981c281c5aa10b61
Phillipp Bertram <phillipp.bertram@gmail.com>
2017-02-12 14:54:08 +0100

    added Gemfile, Swift-version, Unit Tests

---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Demo
+
+end
+
+target 'JsonDiffPatch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for JsonDiffPatch
+
+end
+
+target 'Tests' do
+    # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+    use_frameworks!
+    
+    # Pods for Tests
+    pod 'Nimble', '~> 5.1.1'
+    pod 'Quick', '~> 1.0.0'
+    pod 'SwiftyJSON'
+
+end
---
+++ Tests/JsonDiffPatchTest.swift
@@ -0,0 +1,79 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Phillipp Bertram on 12/02/2017.
+//  Copyright © 2017 Phillipp Bertram. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SwiftyJSON
+
+@testable import JsonDiffPatch
+
+func jsonString(forFileName fileName: String) -> String {
+    guard let path = Bundle(for: JsonDiffPatchSpec.self)
+        .path(forResource: fileName, ofType: "json") else {
+            return ""
+    }
+    
+    do {
+        return try String(contentsOfFile: path)
+    } catch {
+        return ""
+    }
+    
+}
+
+func jsonDict(forFileName fileName: String) -> [String: Any] {
+    let string = jsonString(forFileName: fileName)
+    guard let data = string.data(using: .utf8) else {
+        return [:]
+    }
+    let json = try? JSONSerialization.jsonObject(with: data, options: [])
+    if let json = json as? [String: Any] {
+        return json
+    }
+    return [:]
+}
+
+func asJsonString(dict: [String: Any]) -> String {
+    do {
+        let data = try JSONSerialization.data(withJSONObject: dict)
+        return String(data: data, encoding: .utf8) ?? ""
+    } catch {
+        return ""
+    }
+}
+
+class JsonDiffPatchSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("JsonDiffPatch") {
+            
+            context("diff") {
+                
+                it("complex") {
+                    
+                    // given
+                    let left = jsonString(forFileName: "left")
+                    let right = jsonString(forFileName: "right")
+                    let expected = JSON.parse(jsonString(forFileName: "expected"))
+                
+                    // when
+                    let delta = JsonDiffPatch.diff(source: left, target: right)
+
+                    // then
+                    expect(JSON(delta)) == expected
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
