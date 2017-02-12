c599f30b6b323d22c46c6a666af4e42aa71b6118
Phillipp Bertram <phillipp.bertram@gmail.com>
2017-02-12 10:42:14 +0100

    Added Base JsonDiffPatch class; reorganized Project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonDiffPatchSwift
+//
+//  Created by Phillipp Bertram on 11/02/2017.
+//  Copyright © 2017 Phillipp Bertram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
